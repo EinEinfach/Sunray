5668a35c5c95f189316d54594c81700afe2b4ca5
Alexander Weiss <sanjok@gmx.de>
2023-06-30 21:23:39 +0200

    Comment SUPER_SPIKE_ELIMINATOR

--- sunray/src/driver/AmRobotDriver.cpp
+++ sunray/src/driver/AmRobotDriver.cpp
@@ -21,7 +21,7 @@
 #endif
 
 
-#define SUPER_SPIKE_ELIMINATOR 1  // advanced spike elimination  (experimental, comment out to disable)
+//#define SUPER_SPIKE_ELIMINATOR 1  // advanced spike elimination  (experimental, comment out to disable)
 
 
 volatile int odomTicksLeft  = 0;
@@ -112,7 +112,8 @@ void OdometryMowISR(){
   #else
     motorMowTicksTimeout = millis() + 1;
   #endif
-  odomTicksMow++;    
+  odomTicksMow++;
+  asm("dsb");     
 }
 
 
