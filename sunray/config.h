88729478ea024a489d51b66e1ed9892f69c210e0
EinEinfach <sanjok@gmx.de>
2025-07-30 13:39:20 +0200

    Max number of obstacles added before clearObstacles called

--- sunray/config.h
+++ sunray/config.h
@@ -345,6 +345,7 @@ Also, you may choose the serial port below for serial monitor output (CONSOLE).
 //#define OBSTACLE_AVOIDANCE false  // stop robot on obstacle
 #define OBSTACLE_DIAMETER 1.2   // choose diameter of obstacles placed in front of robot (m) for obstacle avoidance
 #define DISABLE_MOW_MOTOR_AT_OBSTACLE true // switch off mow motor while escape at detected obstacle; set false if mow motor shall not be stopped at detected obstacles
+#define OBSTACLE_MAX_NUMBER 10  // maximum number of obstacles to store in memory (if more obstacles detected, all obstacles will be cleared)
 
 // detect robot being kidnapped? robot will try GPS recovery if distance to tracked path is greater than a certain value
 // (false GPS fix recovery), and if that fails go into error 
